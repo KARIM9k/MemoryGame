c1a9a1a85e33cf05e1cee2f8825bfddae66cdfd5
Ahmad Malak <ahmad3511malak@gmail.com>
2026-09-19 13:19:11 +0300

    hjik

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -121,7 +122,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Myanmar Text", 34.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(565, 335);
+            this.label3.Location = new System.Drawing.Point(521, 339);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(200, 103);
             this.label3.TabIndex = 8;
@@ -136,11 +137,16 @@
             this.label4.Size = new System.Drawing.Size(200, 103);
             this.label4.TabIndex = 9;
             this.label4.Text = "KOKO";
-            this.label3.Location = new System.Drawing.Point(521, 339);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(200, 103);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "KOKO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Myanmar Text", 34.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(308, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 103);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "KOKO";
             // 
             // Form1
             // 
@@ -148,6 +154,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -178,6 +185,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
